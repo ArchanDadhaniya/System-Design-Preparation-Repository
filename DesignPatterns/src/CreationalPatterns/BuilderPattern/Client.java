7a8c4ea3d5620fb52a754422fce3b4eb77808bed
Archan Dadhaniya <archandadhaniya@yahoo.com>
2024-09-24 17:43:12 +0530

    Builder Pattern it has been done.

---
+++ DesignPatterns/src/CreationalPatterns/BuilderPattern/Client.java
@@ -0,0 +1,23 @@
+package CreationalPatterns.BuilderPattern;
+
+public class Client {
+
+    public static void main(String args[]){
+
+        Director directorObj1 = new Director(new EngineeringStudentBuilder());
+        Director directorObj2 = new Director(new MBAStudentBuilder());
+
+        Student engineerStudent = directorObj1.createStudent();
+        Student mbaStudent =directorObj2.createStudent();
+
+        System.out.println(engineerStudent.toString());
+        System.out.println(mbaStudent.toString());
+
+    }
+}
+
+
+/*
+Separate the construction if a complex object from its representation so that the same construction process can
+create different representations.
+ */
