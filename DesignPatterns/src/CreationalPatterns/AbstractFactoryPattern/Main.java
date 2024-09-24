9752a13198e4da8a7b545aec9a6732b0062fa3c4
Archan Dadhaniya <archandadhaniya@yahoo.com>
2024-09-24 11:16:43 +0530

    Add definition of a Factory Method design pattern.

--- DesignPatterns/src/CreationalPatterns/AbstractFactoryPattern/Main.java
+++ DesignPatterns/src/CreationalPatterns/AbstractFactoryPattern/Main.java
@@ -20,3 +20,11 @@ public class Main {
     }
 }
 
+
+
+/*
+Intent:-
+         Define an interface for creating an object, but let subclasses decide which class to instantiate. Factory
+         Method lets a class defer instantiation to subclass.
+ */
+
