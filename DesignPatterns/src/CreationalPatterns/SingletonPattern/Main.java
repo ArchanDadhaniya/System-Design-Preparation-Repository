0991321d9925534278ce620c1778a0067a619dba
Shiva <archandadhaniya@hotmail.com>
2026-09-08 23:22:29 +0530

    Make change in the singleton design pattern main class and add the print statement.

--- DesignPatterns/src/CreationalPatterns/SingletonPattern/Main.java
+++ DesignPatterns/src/CreationalPatterns/SingletonPattern/Main.java
@@ -8,18 +8,22 @@ import CreationalPatterns.SingletonPattern.SynchronizedMethod.DatabaseConnection
 public class Main {
     public static void main(String[] args) {
         // Test Eager Initialization
+        System.out.println("------------------EAGER Initialization------------------");
         DatabaseConnectionEager eagerConnection = DatabaseConnectionEager.getInstance();
         eagerConnection.connect();
 
         // Test Lazy Initialization
+        System.out.println("------------------Lazy Initialization------------------");
         DatabaseConnectionLazy lazyConnection = DatabaseConnectionLazy.getInstance();
         lazyConnection.connect();
 
         // Test Synchronized Method
+        System.out.println("------------------Synchronized Initialization------------------");
         DatabaseConnectionSynchronized synchronizedConnection = DatabaseConnectionSynchronized.getInstance();
         synchronizedConnection.connect();
 
         // Test Double-Checked Locking
+        System.out.println("------------------Double-Checked Initialization------------------");
         DatabaseConnectionDoubleChecked doubleCheckedConnection = DatabaseConnectionDoubleChecked.getInstance();
         doubleCheckedConnection.connect();
     }
